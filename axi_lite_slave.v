2176f962163bbcd7db509e59b4ebff3eee8740df
Anant Anurag <anantquantumphysics@gmail.com>
2020-11-08 20:53:32 +0530

    Made new folder run_test to run simulations but added it inside gitignore file

--- axi_lite_slave.v
+++ axi_lite_slave.v
@@ -52,69 +52,78 @@ module axi_lite_slave_read (
 	input S_AXIL_RREADY,
 	output S_AXIL_RDATA,
 	output S_AXIL_RRESP,
+
+	// User Port Signals Start
+	input user_port_arready,
+	output user_port_arvalid,
+	input user_port_rvalid,
+	input user_port_rdata,
+	input user_port_rresp,
+	output user_port_araddr
+	// User Port Signals End
 );
 
-// Interface Signals Begin
-wire S_AXIL_ACLK;
-wire S_AXIL_ARESETn;
+	// Interface Signals Begin
+	wire S_AXIL_ACLK;
+	wire S_AXIL_ARESETn;
 
-reg S_AXIL_ARVALID;
-reg S_AXIL_ARREADY;
-reg S_AXIL_ARADDR[31:0];
-reg S_AXIL_ARPROT[2:0];
-reg S_AXIL_RVALID;
-reg S_AXIL_RREADY;
-reg S_AXIL_RDATA[31:0];
-reg S_AXIL_RRESP[1:0];
-// Interface Signals End
-
-
-// Signals going to Functional Block to drive Relevant Data Start
-reg user_port_arready;
-reg user_port_arvalid; // Maybe slave needs to do this Slave can wait for ARVALID before asserting ARREADY
-reg user_port_rvalid;
-reg user_port_rdata[31:0];
-reg user_port_rresp;
-reg user_port_araddr[31:0];
-// Signals going to Functional Block to drive Relevant Data End
-
-// FSM Signals Start
-reg fsm_saxil_current_state [1:0];
-reg fsm_saxil_next_state [1:0];
-// FSM Signals End
-
-initial begin
-	//All Signals equal to zero
-	S_AXIL_ARVALID = 1'b0;
-	S_AXIL_ARREADY = 1'b0;
-	S_AXIL_ARADDR = 32'b0;
-	S_AXIL_ARPROT = 3'b0;
-	S_AXIL_RVALID = 1'b0;
-	S_AXIL_RREADY = 1'b0;
-	S_AXIL_RDATA = 32'b0;
-	S_AXIL_RRESP = 2'b0;
-end
+	reg S_AXIL_ARVALID;
+	reg S_AXIL_ARREADY;
+	reg S_AXIL_ARADDR[31:0];
+	reg S_AXIL_ARPROT[2:0];
+	reg S_AXIL_RVALID;
+	reg S_AXIL_RREADY;
+	reg S_AXIL_RDATA[31:0];
+	reg S_AXIL_RRESP[1:0];
+	// Interface Signals End
 
 
-always @ (~S_AXIL_ARESETn) begin
-	// ***** ASYNCHRONOUS ***** 
-	// At Reset, make all output signals 0
-	S_AXIL_ARREADY = 1'b0;
-	S_AXIL_AXIL_RVALID = 1'b0;
-	S_AXIL_RDATA = 32'b0;
-	S_AXIL_RRESP = 2'b0;
-end
+	// Signals going to Functional Block to drive Relevant Data Start
+	reg user_port_arready;
+	reg user_port_arvalid; // Maybe slave needs to do this Slave can wait for ARVALID before asserting ARREADY
+	reg user_port_rvalid;
+	reg user_port_rdata[31:0];
+	reg user_port_rresp;
+	reg user_port_araddr[31:0];
+	// Signals going to Functional Block to drive Relevant Data End
+
+	// FSM Signals Start
+	reg fsm_saxil_current_state [1:0];
+	reg fsm_saxil_next_state [1:0];
+	// FSM Signals End
+
+	initial begin
+		//All Signals equal to zero
+		S_AXIL_ARVALID = 1'b0;
+		S_AXIL_ARREADY = 1'b0;
+		S_AXIL_ARADDR = 32'b0;
+		S_AXIL_ARPROT = 3'b0;
+		S_AXIL_RVALID = 1'b0;
+		S_AXIL_RREADY = 1'b0;
+		S_AXIL_RDATA = 32'b0;
+		S_AXIL_RRESP = 2'b0;
+	end
 
 
-// Actual Logic starts here 
-/** Finite State Machine
-States - 
-	0. FSM_SAXIL_RD_WAIT_FOR_ARVALID_ASSERTION - no action or after tx complete
-	1. FSM_SAXIL_RD_ACCEPT_ADDRESS_FROM_BUS - arvalid is 1
-	2. FSM_SAXIL_RD_WAIT_FOR_ARREADY_ASSERTION - wait state for user device to be ready to accept addr
-	3. FSM_SAXIL_RD_PROCURE_DATA_FROM_DEVICE - addr is accepted and arready is 1
-	4. FSM_SAXIL_RD_WAIT_FOR_RREADY_ASSERTION - wait till master asserts rready
-	5. FSM_SAXIL_RD_SEND_DATA_TO_BUS - rvalid is 1
+	always @ (~S_AXIL_ARESETn) begin
+		// ***** ASYNCHRONOUS ***** 
+		// At Reset, make all output signals 0
+		S_AXIL_ARREADY = 1'b0;
+		S_AXIL_AXIL_RVALID = 1'b0;
+		S_AXIL_RDATA = 32'b0;
+		S_AXIL_RRESP = 2'b0;
+	end
+
+
+	// Actual Logic starts here 
+	/** Finite State Machine
+	States - 
+		0. FSM_SAXIL_RD_WAIT_FOR_ARVALID_ASSERTION - no action or after tx complete
+		1. FSM_SAXIL_RD_ACCEPT_ADDRESS_FROM_BUS - arvalid is 1
+		2. FSM_SAXIL_RD_WAIT_FOR_ARREADY_ASSERTION - wait state for user device to be ready to accept addr
+		3. FSM_SAXIL_RD_PROCURE_DATA_FROM_DEVICE - addr is accepted and arready is 1
+		4. FSM_SAXIL_RD_WAIT_FOR_RREADY_ASSERTION - wait till master asserts rready
+		5. FSM_SAXIL_RD_SEND_DATA_TO_BUS - rvalid is 1
 
 **/
 always @ (posedge(S_AXIL_ACLK) && S_AXIL_ARESETn) begin
